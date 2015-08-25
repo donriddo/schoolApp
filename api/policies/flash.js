105a9bcad8ec448377e6b055af1b40676a344df2
donriddo <donriddo@gmail.com>
2015-08-25 16:23:44 +0100

    updated

--- api/policies/flash.js
+++ api/policies/flash.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
       return next();
     } else {
       res.locals.flash = _.clone(req.session.flash);
-      req.session.flash = {};
+      req.session.flash = {}; // this clears the flash session to ensure error doesn't persist
     }
 
     next();
